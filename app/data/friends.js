1e9cf824845ca50141e5834204c3f663dbce1921
Geralyn E Craig <44543127+gericraig@users.noreply.github.com>
2019-02-06 00:49:46 -0800

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,163 +1,55 @@
 // Data structure that will store the friends data
-var friendsArray = [
-	{
-		name: 'Joker',
+var friends = [{
+		name: 'The Joker',
 		photo: 'https://www.syfy.com/sites/syfy/files/wire/legacy/Hamill-Joker.jpg',
-		scores: [
-			'1',
-			'1',
-			'5',
-			'1',
-			'3',
-			'2',
-			'1',
-			'1',
-			'1',
-			'5'
-		]
+		scores: [1,1,5,1,3,2,1,1,1,5]
 	},
 	{
 		name: 'Bruce Wayne',
 		photo: 'https://www.syfy.com/sites/syfy/files/styles/1200x680/public/syfywire_blog_post/2018/09/michael_keaton.jpg?itok=1BCfHNP9',
-		scores: [
-			'5',
-			'5',
-			'1',
-			'2',
-			'1',
-			'4',
-			'4',
-			'2',
-			'1',
-			'5'
-		]
+		scores: [5,5,1,2,1,4,4,2,1,5]
 	},
 	{
-		name: 'Harleen Quinnzel',
+		name: 'Harley Quinn',
 		photo: 'https://vignette.wikia.nocookie.net/batmantheanimatedseries/images/8/82/Harleyquinade_Radomski.jpg/revision/latest?cb=20180706235625',
-		scores: [
-			'4',
-			'3',
-			'4',
-			'3',
-			'4',
-			'5',
-			'5',
-			'1',
-			'1',
-			'1'
-		]
+		scores: [4,3,4,3,4,5,5,1,1,1,]
 	},
 	{
-		name: 'Johnathan Crane',
+		name: 'Scarecrow',
 		photo: 'https://cdn3.whatculture.com/images/2016/08/36f326284a1b7160-600x338.jpg',
-		scores: [
-			'5',
-			'1',
-			'4',
-			'1',
-			'5',
-			'1',
-			'1',
-			'3',
-			'3',
-			'4'
-		]
+		scores: [5,1,4,1,5,1,1,3,3,4]
+	
 	},
 	{
 		name: 'Dick Grayson',
 		photo: 'https://vignette.wikia.nocookie.net/batman/images/b/b7/Nightwing133.jpg/revision/latest?cb=20111119192941',
-		scores: [
-			'3',
-			'5',
-			'1',
-			'5',
-			'1',
-			'5',
-			'4',
-			'5',
-			'5',
-			'1'
-		]
+		scores: [3,5,1,5,1,5,4,5,5,1]
 	},
 	{
 		name: 'Barbara Gordon',
 		photo: 'http://image.wikifoundry.com/image/1/ftF8BTunHOUzkWLV-_iFmw33233/GW400H400',
-		scores: [
-			'2',
-			'4',
-			'1',
-			'5',
-			'1',
-			'5',
-			'5',
-			'1',
-			'1',
-			'5'
-		]
+		scores: [2,4,1,5,1,5,5,1,1,5]
 	},
 	{
-		name: 'Harvey Dent',
+		name: 'Two-Face',
 		photo: 'https://vignette.wikia.nocookie.net/hohrpgseries/images/0/0e/Two-Face_%28S1-S1%29.jpg/revision/latest/scale-to-width-down/305?cb=20091213024613',
-		scores: [
-			'3',
-			'3',
-			'3',
-			'3',
-			'3',
-			'3',
-			'3',
-			'3',
-			'3',
-			'3'
-		]
+		scores: [3,3,3,3,3,3,3,3,3,3]
 	},
 	{
 		name: 'Alfred Pennyworth',
 		photo: 'https://img.purch.com/o/aHR0cDovL3d3dy5uZXdzYXJhbWEuY29tL2ltYWdlcy9pLzAwMC8yMjYvOTc2L2kwMi9BbGZyZWQuanBn',
-		scores: [
-			'1',
-			'4',
-			'1',
-			'5',
-			'1',
-			'5',
-			'3',
-			'1',
-			'1',
-			'5'
-		]
+		scores: [1,4,1,5,1,5,3,1,1,5]
 	},
 	{
-		name: 'Pamela Isley',
+		name: 'Poison Ivy',
 		photo: 'https://i.pinimg.com/originals/35/56/fb/3556fbf7e92315f2c4b777b029b80536.jpg',
-		scores: [
-			'5',
-			'2',
-			'2',
-			'1',
-			'3',
-			'2',
-			'1',
-			'5',
-			'5',
-			'5'
-		]
+		scores: [5,2,2,1,3,2,1,5,5,5]
 	},
 	{
 		name: 'Selina Kyle',
 		photo: 'https://imgix-media.wbdndc.net/cms/filer_public/61/35/6135e979-ff45-4de0-9809-c7b76a729bab/header-batmanreturns_stills-17.jpg',
-		scores: [
-			'4',
-			'3',
-			'1',
-			'4',
-			'1',
-			'3',
-			'3',
-			'1',
-			'1',
-			'1'
-		]
+		scores: [4,3,1,4,1,3,3,1,1,1]
 	}
 ];
+
+module.exports = friends;
