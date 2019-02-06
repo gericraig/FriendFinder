e3f14a8b4e64e8ba2d5998d8ceebed6bb2cbd59b
Geralyn E Craig <44543127+gericraig@users.noreply.github.com>
2019-02-06 00:53:39 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
 
-// Configure the Express application
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
 var app = express();
-var PORT = process.env.PORT;
 
-// Expose the public directory to access CSS files
-app.use(express.static(path.join(__dirname, './app/public')));
+var PORT = process.env.PORT || 3000;
 
-// Add middleware for parsing incoming request bodies
+// For serving of static CSS
+app.use(express.static(__dirname + "/app/css"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Add the application routes
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-// Start listening on PORT
 app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+	console.log("App listening on PORT: " + PORT);
 });
